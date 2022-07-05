a69ed9fb2cb06e62dd74539530a5d431a70b9c5b
asrarbutt <asrarbutt1986@gmail.com>
2022-07-05 16:35:34 +0200

    implements interfaces in Smartphone

--- src/main/java/Model/Smartphone.java
+++ src/main/java/Model/Smartphone.java
@@ -1,4 +1,24 @@
 package Model;
 
-public class Smartphone {
+public class Smartphone implements Radio, GPS{
+    private String modellName, producerName;
+    private Contact [] contactsArray;
+
+
+
+    @Override
+    public String getPosition() {
+        return null;
+    }
+
+    @Override
+    public boolean startRadio() {
+        System.out.println("Radio started");
+        return false;
+    }
+
+    @Override
+    public boolean stopRadio() {
+        return false;
+    }
 }
