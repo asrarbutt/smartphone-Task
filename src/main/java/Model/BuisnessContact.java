d66ad9f5dd18a76d78d5bef14b75a0891f33e411
asrarbutt <asrarbutt1986@gmail.com>
2022-07-05 17:04:20 +0200

    add constructor

--- src/main/java/Model/BuisnessContact.java
+++ src/main/java/Model/BuisnessContact.java
@@ -2,8 +2,38 @@ package Model;
 
 public class BuisnessContact extends Contact {
 
+    private String companyName;
+
+
+
+    public BuisnessContact(String companyName) {
+        this.companyName = companyName;
+    }
+    public BuisnessContact( ) {
+
+    }
+
+    public BuisnessContact(String contactName, String companyName) {
+        super(contactName);
+        this.companyName = companyName;
+    }
+
+
+    //Getter Setter
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    //extend  Method from Contact
     @Override
     public String getName(String contactName) {
         return null;
     }
+
+
 }
