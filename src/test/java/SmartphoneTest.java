9f13af3841d7207af1f0f46c3ccea8bd26917236
asrarbutt <asrarbutt1986@gmail.com>
2022-07-05 16:57:40 +0200

    finish test for Smartphone class

--- src/test/java/SmartphoneTest.java
+++ src/test/java/SmartphoneTest.java
@@ -1,2 +1,50 @@
+import Model.Radio;
+import Model.Smartphone;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SmartphoneTest {
+
+    Smartphone smartphone=new Smartphone();
+
+    @Test
+    void startRadioTest(){
+        //given
+        boolean startRadio=true;
+
+        //when
+        boolean actual=smartphone.startRadio();
+        boolean exspected=true;
+
+        //then
+        Assertions.assertEquals(exspected, actual);
+
+
+    }
+
+    @Test
+    void stoppRadioTest(){
+        //given
+        boolean startRadio=true;
+
+        //when
+        boolean actual=smartphone.startRadio();
+        boolean exspected=false;
+
+        //then
+        Assertions.assertEquals(exspected, actual);
+    }
+
+    @Test
+    void gpsPositionTest(){
+        //given
+        String position="Hamburg";
+
+        //when
+        String actual=smartphone.getPosition(position);
+        String exspected="Hamburg";
+
+        //then
+        Assertions.assertEquals(exspected, actual);
+    }
 }
